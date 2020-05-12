b598ee82197de7c3bdaa4d400df5f13ec9c4d908
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-05-12 11:26:48 +0900

    c# 0512 6번 문제

--- startree/Program.cs
+++ startree/Program.cs
@@ -10,6 +10,18 @@ namespace startree
     {
         static void Main(string[] args)
         {
+            for(int i=0; i < 8; i++)
+            {
+                for(int j = 0; j < 8-i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int z = 0; z <=i*2; z++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
